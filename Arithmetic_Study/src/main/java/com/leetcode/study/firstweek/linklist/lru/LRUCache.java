25045811eb11608ed9dbc490ce5df372be6d6252
xuanzhirong <1040775954@qq.com>
2021-04-09 00:58:11 +0800

    146 LRU缓存机制

--- Arithmetic_Study/src/main/java/com/leetcode/study/firstweek/linklist/lru/LRUCache.java
+++ Arithmetic_Study/src/main/java/com/leetcode/study/firstweek/linklist/lru/LRUCache.java
@@ -1,4 +1,4 @@
-package com.leetcode.study.firstweek.linklist;
+package com.leetcode.study.firstweek.linklist.lru;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -14,10 +14,12 @@ public class LRUCache extends LinkedHashMap<Integer, Integer> {
         this.capacity = capacity;
     }
 
+    // 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
     public int get(int key) {
         return super.getOrDefault(key, -1);
     }
 
+    // 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
     public void put(int key, int value) {
         super.put(key, value);
     }
