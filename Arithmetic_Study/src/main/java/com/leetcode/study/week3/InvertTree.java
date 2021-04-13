d6c0ec1adbe5b2695bf79332f893562399eb9498
xuanzhirong <1040775954@qq.com>
2021-04-13 21:00:37 +0800

    226. 翻转二叉树

---
+++ Arithmetic_Study/src/main/java/com/leetcode/study/week3/InvertTree.java
@@ -0,0 +1,37 @@
+package com.leetcode.study.week3;
+
+/**
+ * 226. 翻转二叉树
+ */
+public class InvertTree {
+    // 记住了递归模板后，分析得到大问题分解为子问题，递归方法自己做了出来
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
