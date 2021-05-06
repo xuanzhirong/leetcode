538fad6e360a2ee4c6e59e858bfc403b34090333
xuanzhirong <1040775954@qq.com>
2021-05-07 00:24:57 +0800

    206. 反转链表

--- Arithmetic_Study/src/main/java/com/leetcode/study/week1/linklist/ReverseList.java
+++ Arithmetic_Study/src/main/java/com/leetcode/study/week1/linklist/ReverseList.java
@@ -1,5 +1,8 @@
 package com.leetcode.study.week1.linklist;
 
+import java.util.Stack;
+import java.util.zip.CRC32;
+
 /**
  * 206. 反转链表
  * <p>
@@ -11,8 +14,40 @@ package com.leetcode.study.week1.linklist;
  * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
  */
 public class ReverseList {
+    // 迭代
     public ListNode reverseList(ListNode head) {
-        return head;
+        //申请节点，pre和 cur，pre指向null
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode tmp = null;
+        while(cur!=null) {
+            //记录当前节点的下一个节点
+            tmp = cur.next;
+            //然后将当前节点指向pre
+            cur.next = pre;
+            //pre和cur节点都前进一位
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+
+    // 递归
+    public ListNode reverseList2(ListNode head) {
+        //递归终止条件是当前为空，或者下一个节点为空
+        if(head==null || head.next==null) {
+            return head;
+        }
+        //这里的cur就是最后一个节点
+        ListNode cur = reverseList(head.next);
+        //如果链表是 1->2->3->4->5，那么此时的cur就是5
+        //而head是4，head的下一个是5，下下一个是空
+        //所以head.next.next 就是5->4
+        head.next.next = head;
+        //防止链表循环，需要将head.next设置为空, 即 5->4->null
+        head.next = null;
+        //每层递归函数都返回cur，也就是最后一个节点
+        return cur;
     }
 
     public class ListNode {
