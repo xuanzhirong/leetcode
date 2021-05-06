98ff225eb02410317f61a14797dea290a653c861
xuanzhirong <1040775954@qq.com>
2021-05-06 21:37:37 +0800

    53. 最大子序和---动态规划

---
+++ Arithmetic_Study/src/main/java/com/leetcode/study/dailypractice/MaxSubArray.java
@@ -0,0 +1,31 @@
+package com.leetcode.study.dailypractice;
+
+/**
+ * 53. 最大子序和
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * <p>
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ */
+public class MaxSubArray {
+    // 动态规划
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            if (sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(new MaxSubArray().maxSubArray(nums));
+    }
+}
