97dddc09ad1efd3c2b10296a2b09f058c5926145
xuanzhirong <1040775954@qq.com>
2021-05-07 23:34:33 +0800

    62. 不同路径  -- 动规

---
+++ Arithmetic_Study/src/main/java/com/leetcode/study/week6/UniquePaths.java
@@ -0,0 +1,40 @@
+package com.leetcode.study.week6;
+
+/**
+ * 62. 不同路径
+ */
+public class UniquePaths {
+    // 自底向上
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 || j == n - 1) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+    // 自顶向下
+    public int uniquePaths2(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new UniquePaths().uniquePaths(3, 7));
+    }
+}
