03dbb1fc2b60d4e276b5eda452bb670a9c732f77
xuanzhirong <1040775954@qq.com>
2021-05-12 23:58:59 +0800

    141. 环形链表

---
+++ Arithmetic_Study/src/main/java/com/leetcode/study/week1/linklist/HasCycle.java
@@ -0,0 +1,33 @@
+package com.leetcode.study.week1.linklist;
+
+/**
+ * 141. 环形链表
+ */
+public class HasCycle {
+    // 快慢指针，快指针追上慢指针，说明有环
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
